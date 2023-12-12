e98a95e7f73dd69bfbb0f7e104c2d5c1c6a5acde
Vladislav <labkovich.vl@gmail.com>
2023-12-12 14:23:44 +0300

    feat: add array

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,11 +4,13 @@ import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 import {Main} from "../section/main/Main";
 
+const items = ['Home','Skills','Works','Testimony','Contact'];
+
 export const Header = () => {
     return (
         <StyledHeader>
             <Logo/>
-            <Menu/>
+            <Menu menuItems={items}/>
         </StyledHeader>
     );
 };
@@ -17,5 +19,4 @@ const StyledHeader = styled.header`
   background-color: #a7d2a7;
   display: flex;
   justify-content: space-between;
-  
 `
