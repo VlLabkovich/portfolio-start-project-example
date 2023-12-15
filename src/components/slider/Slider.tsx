8fb93bfc5597ab4be68752793e41347e124986cb
Vladislav <labkovich.vl@gmail.com>
2023-12-15 13:28:05 +0300

    feat: add component

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../FlexWrapper";
+
+export const Slider = () => {
+    return (
+        <StyledSlider>
+            <FlexWrapper>
+                <Slide>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.</Text>
+                    <Name>@ivan ivanow</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span></span>
+                <span></span>
+                <span></span>
+            </Pagination>
+        </StyledSlider>
+    );
+};
+
+const StyledSlider = styled.div`
+    border: 4px solid red;
+    max-width: 500px;
+`
+
+const Slide = styled.div`
+    text-align: center;
+`
+
+const Text = styled.p`
+
+`
+
+const Name = styled.span`
+
+`
+
+const Pagination = styled.span`
+    display: flex;
+    justify-content: center;
+    span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        border: 2px solid green;
+        margin: 5px;
+        background-color: green;
+
+    }
+`
