298c9003ebc4a904eabe94568c0c96aefeb9fe43
Vladislav <labkovich.vl@gmail.com>
2023-12-16 01:09:37 +0300

    feat: add component

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../../components/icon/Icon";
+import {FlexWrapper} from "../../components/FlexWrapper";
+
+export const Footer = () => {
+    return (
+        <StyledFooter>
+            <Name>Svetlana</Name>
+            <FlexWrapper direction={'column'} align={'center'}>
+                <SocialList>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'instagram'}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'telegram'}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'vk'}/>
+                        </SocialLink>
+                    </SocialItem>
+                    <SocialItem>
+                        <SocialLink>
+                            <Icon height={'21px'} width={'21px'} viewBox={'0 0 21px 21px'} iconId={'linkedin'}/>
+                        </SocialLink>
+                    </SocialItem>
+                </SocialList>
+                <Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</Copyright>
+            </FlexWrapper>
+        </StyledFooter>
+    )
+        ;
+};
+
+const StyledFooter = styled.footer`
+    min-height: 30px;
+    background-color: grey;
+
+`
+
+const Name = styled.span`
+    display: flex;
+    justify-content: center;
+`
+
+const SocialList = styled.ul`
+    display: flex;
+    gap: 30px;
+`
+
+const SocialItem = styled.li`
+
+    list-style: none;
+`
+
+const SocialLink = styled.a`
+
+`
+
+const Copyright = styled.small`
+
+`
