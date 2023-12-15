eadfc28859eef6a222950947d665e0394c9191b6
Vladislav <labkovich.vl@gmail.com>
2023-12-15 13:29:12 +0300

    feat: add component

---
+++ src/layout/section/testimony/Testimony.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Icon} from "../../../components/icon/Icon";
+import {Slider} from "../../../components/slider/Slider";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <FlexWrapper direction={'column'} align={'center'}>
+                <Icon iconId={'quoteAltRight'}/>
+                <Slider/>
+            </FlexWrapper>
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+    min-height: 50vh;
+    background-color: cornflowerblue;
+    border: 4px dashed rgba(0, 0, 0, 0.79);
+`
