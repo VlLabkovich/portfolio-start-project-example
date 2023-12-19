dd29c80281266799d71dba87217ca09c8883e77d
Vladislav <labkovich.vl@gmail.com>
2023-12-19 23:40:05 +0300

    feat: add styles

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -14,12 +14,13 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         color: ${theme.colors.font};
+        line-height: 1.2;
     }
 
     a {
