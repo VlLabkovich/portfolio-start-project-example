424a0d9d185c71a76560812aaff0b97b56084377
Vladislav <labkovich.vl@gmail.com>
2023-12-21 01:21:23 +0300

    feat: add style element

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -36,6 +36,10 @@ export const GlobalStyle = createGlobalStyle`
         border: none;
     }
     
+    section {
+        padding: 100px 0;
+    }
+    
     section:nth-of-type(odd) {
         background-color: ${theme.colors.primaryBg};
     }
