b7c24f733898fa985b727e3f1bdc1a661156a14f
Vladislav <labkovich.vl@gmail.com>
2023-12-24 01:08:51 +0300

    feat: add styles

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Slider = () => {
     return (
@@ -10,12 +11,12 @@ export const Slider = () => {
                     <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit.</Text>
-                    <Name>@ivan ivanow</Name>
+                    <Name>@ivan ivanov</Name>
                 </Slide>
             </FlexWrapper>
             <Pagination>
                 <span></span>
-                <span></span>
+                <span className={"active"}></span>
                 <span></span>
             </Pagination>
         </StyledSlider>
@@ -35,19 +36,33 @@ const Text = styled.p`
 `
 
 const Name = styled.span`
-
+    display: inline-block;
+    margin: 22px 0 42px 0;
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: 1px;
+    text-transform: uppercase;
 `
 
 const Pagination = styled.span`
     display: flex;
     justify-content: center;
+
     span {
         display: inline-block;
-        width: 10px;
-        height: 10px;
-        border: 2px solid green;
-        margin: 5px;
-        background-color: green;
+        width: 7px;
+        height: 7px;
+        border-radius: 20px;
+        background: rgba(255, 255, 255, 0.50);
 
+        & + span {
+            margin-left: 5px;
+        }
+
+        &.active {
+            background-color: ${theme.colors.accent};
+            width: 20px;
+        }
     }
 `
