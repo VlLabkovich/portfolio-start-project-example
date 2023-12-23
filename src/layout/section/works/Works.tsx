3b927d0f1947cf032162c584e013e3fe66775970
Vladislav <labkovich.vl@gmail.com>
2023-12-23 23:50:00 +0300

    feat: add styles components

--- src/layout/section/works/Works.tsx
+++ src/layout/section/works/Works.tsx
@@ -4,18 +4,20 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import socialImg from '../../../assets/images/proj-1.webp'
 import timerImg from '../../../assets/images/proj-2.webp'
-import {Menu} from "../../../components/menu/Menu";
+import {TabMenu} from "./tabMenu/TabMenu";
 import {Work} from "./work/Work";
+import {Container} from "../../../components/Container";
 
 
-const workItems = ['All','landing page','React','spa'];
+const workItems = ['All', 'landing page', 'React', 'spa'];
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>My works</SectionTitle>
-            <Menu menuItems={workItems} />
-            <FlexWrapper justify={'space-around'}>
+            <Container>
+                <SectionTitle>My works</SectionTitle>
+                <TabMenu menuItems={workItems}/>
+                <FlexWrapper justify={'space-between'} align={'flex-start'}>
                     <Work title={'Social Network'}
                           src={socialImg}
                           text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}
@@ -24,25 +26,11 @@ export const Works = () => {
                           src={timerImg}
                           text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim'}
                     />
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-  background-color: #43d4e8;
-  min-height: 100vh;
-`;
-
-const Photo = styled.img`
-  width: 500px;
-  min-height: 500px;
-  object-fit: cover;
-`
-const StyledWork = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  
 `
