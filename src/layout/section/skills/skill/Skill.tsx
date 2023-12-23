890bf2a66ac0367bcee391890fd53b87b09a5234
Vladislav <labkovich.vl@gmail.com>
2023-12-23 23:39:20 +0300

    refactor: rename styles components

--- src/layout/section/skills/skill/Skill.tsx
+++ src/layout/section/skills/skill/Skill.tsx
@@ -16,8 +16,8 @@ export const Skill = (props: SkillPropsType) => {
                 <IconWrapper>
                     <Icon iconId={props.iconId}/>
                 </IconWrapper>
-                <StyledTitle>{props.title}</StyledTitle>
-                <StyledText>{props.description}</StyledText>
+                <SkillTitle>{props.title}</SkillTitle>
+                <SkillText>{props.description}</SkillText>
             </FlexWrapper>
         </StyledSkill>
     );
@@ -46,18 +46,10 @@ position: relative;
     
 }
 `
-const StyledTitle = styled.h3`
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 16px;
-    font-weight: 700;
+const SkillTitle = styled.h3`
     margin: 70px 0 15px;
-
-    letter-spacing: 1px;
     text-transform: uppercase;
 `
-const StyledText = styled.p`
+const SkillText = styled.p`
     text-align: center;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.4;
 `
