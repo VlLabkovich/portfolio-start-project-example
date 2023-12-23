bf8a7c75f65f145cb7ad14c70765d0e8b79399a7
Vladislav <labkovich.vl@gmail.com>
2023-12-23 23:46:10 +0300

    feat: add styles components

--- src/layout/section/works/work/Work.tsx
+++ src/layout/section/works/work/Work.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Link} from "../../../../components/Link";
+import {theme} from "../../../../styles/Theme";
+import {Button} from "../../../../components/Button";
 
 
 type WorkPropsType = {
@@ -11,34 +14,82 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWork>
-            <Image src={props.src} alt=""/>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <Link href={"#"}>demo</Link>
-            <Link href={"#"}>code</Link>
+            <ImageWrapper>
+                <Image src={props.src} alt=""/>
+                <Button>view project</Button>
+            </ImageWrapper>
+            <Description>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <Link href={"#"}>demo</Link>
+                <Link href={"#"}>code</Link>
+            </Description>
         </StyledWork>
     );
 };
 
 const StyledWork = styled.div`
-  max-width: 540px;
-  width: 100%;
-  background-color: aquamarine;
+    max-width: 540px;
+    width: 100%;
+    background: ${theme.colors.secondaryBg};
+
+    ${Link} {
+        padding: 10px 0;
+
+        & + ${Link} {
+            margin-left: 20px;
+        }
+    }
 `
+
+const ImageWrapper = styled.div`
+    position: relative;
+
+    &:hover {
+        &::before {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            background: rgba(0, 0, 0, 0.3);
+            backdrop-filter: blur(4px);
+        }
+
+        ${Button} {
+            opacity: 1;
+        }
+    }
+
+    ${Button} {
+        opacity: 0;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        
+        &::before {
+            width: 100%;
+            height: 100%;
+        }
+    } // центрирование кнопки
+`
+
 const Image = styled.img`
-  width: 100%;
-  height: 260px;
-  object-fit: cover;
+    width: 100%;
+    height: 260px;
+    object-fit: cover;
 `
 
 const Title = styled.h3`
 
 `
 
-const Link = styled.a`
-
+const Text = styled.p`
+    margin: 14px 0 20px;
 `
 
-const Text = styled.p`
-
+const Description = styled.div`
+    padding: 25px 20px;
 `
