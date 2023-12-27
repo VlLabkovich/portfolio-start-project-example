bed5a65b1033c2c880a61381ef84793a1e275951
Vladislav <labkovich.vl@gmail.com>
2023-12-27 23:44:58 +0300

    feat: add media

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -4,12 +4,13 @@ import photo from '../../../assets/images/photo.jpeg'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {theme} from "../../../styles/Theme";
+import {font} from "../../../styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={'wrap'}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Svetlana Dyablo</span></Name>
@@ -31,19 +32,28 @@ const StyledMain = styled.section`
 `
 
 const PhotoWrapper = styled.div`
-position: relative;
+    position: relative;
     z-index: 0;
-    
+    margin-top: 65px;
+
     &::before {
         content: "";
         width: 360px;
         height: 470px;
         border: 5px solid ${theme.colors.accent};
+
         position: absolute;
         top: -24px;
         left: 24px;
         z-index: -1;
         
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -20px;
+            left: 17px;
+            
+        }
     }
 `
 
@@ -51,23 +61,35 @@ const Photo = styled.img`
     width: 350px;
     height: 430px;
     object-fit: cover;
+    margin-right: 15px;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+
+
+    }
 `
 
 const MainTitle = styled.h1`
-    font-size: 27px;
-    font-weight: 400;
+    //font-size: 27px;
+    //font-weight: 400;
+    ${font({weight: 400, Fmax: 27, Fmin: 20})}
+
 `
 
 const Name = styled.h2`
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 50px;
-    font-weight: 700;
+    ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
+    //font-family: 'Josefin Sans', sans-serif;
+    //font-size: 50px;
+    //font-weight: 700;
     letter-spacing: 0.05em;
     margin: 10px 0;
 
     span {
         position: relative;
         z-index: 0;
+        white-space: nowrap;
 
         &::before {
             position: absolute;
@@ -80,6 +102,10 @@ const Name = styled.h2`
             bottom: 0;
             z-index: -1;
         }
+    }
+    
+    @media ${theme.media.mobile} {
+        margin: 15px 0 22px;
     }
 `
 
