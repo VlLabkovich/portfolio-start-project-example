7672e0dd8631391df89fce5f0b857839629b768b
Vladislav <labkovich.vl@gmail.com>
2023-12-29 17:50:54 +0300

    feat: add media

--- src/styles/Global.styled.tsx
+++ src/styles/Global.styled.tsx
@@ -8,7 +8,9 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-    }   // занулили стили  для всех элементов
+    }
+
+    // занулили стили  для всех элементов
 
     body {
         margin: 0;
@@ -40,6 +42,10 @@ export const GlobalStyle = createGlobalStyle`
 
     section {
         padding: 100px 0;
+
+        @media ${theme.media.mobile} {
+            padding: 80px 0;
+        }
     }
 
     section:nth-of-type(odd) {
