66bbd15e562885d9792666e7ee5273a86b9ab56a
Vladislav <labkovich.vl@gmail.com>
2024-01-04 11:34:53 +0300

    refactor: change style

--- src/layout/section/works/tabMenu/TabMenu.tsx
+++ src/layout/section/works/tabMenu/TabMenu.tsx
@@ -21,7 +21,7 @@ const StyledTabMenu = styled.nav`
 
     ul {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
         max-width: 352px;
         width: 100%;
         margin: 0 auto 40px;
