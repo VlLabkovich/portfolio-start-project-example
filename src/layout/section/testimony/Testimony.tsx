b525ccc39095789f2e910ae3312b3ef87a361064
Vladislav <labkovich.vl@gmail.com>
2024-01-08 23:22:21 +0300

    refactor: change structure component

--- src/layout/section/testimony/Testimony.tsx
+++ src/layout/section/testimony/Testimony.tsx
@@ -4,19 +4,19 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {IconWrapper} from "../skills/skill/Skill";
 import {theme} from "../../../styles/Theme";
 import {Container} from "../../../components/Container";
+import {S} from "../skills/Skills_Styles"
 
-export const Testimony = () => {
+export const Testimony: React.FC = () => {
     return (
         <StyledTestimony>
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction={'column'} align={'center'}>
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId={'quoteAltRight'}/>
-                    </IconWrapper>
+                    </S.IconWrapper>
                     <Slider/>
                 </FlexWrapper>
             </Container>
@@ -25,10 +25,9 @@ export const Testimony = () => {
 };
 
 const StyledTestimony = styled.section`
-    ${IconWrapper} {
+    ${S.IconWrapper} {
         margin: 42px 0 72px 0;
-
-
+        
         @media ${theme.media.mobile} {
             margin-top: 13px;
         }
