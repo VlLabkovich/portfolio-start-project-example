ecbf86ebb3018376802f4960fe3d042847798f18
Vladislav <labkovich.vl@gmail.com>
2024-01-09 00:49:06 +0300

    refactor: change type components

--- src/components/SectionTitle.ts
+++ src/components/SectionTitle.ts
@@ -5,9 +5,6 @@ import {font} from "../styles/Common";
 export const SectionTitle = styled.h2`
     ${font({family: "'Josefin Sans', sans-serif", weight: 600, Fmax: 36, Fmin: 30})}
     text-align: center;
-    //font-family: 'Josefin Sans', sans-serif;
-    //font-size: 36px;
-    //font-weight: 600;
     letter-spacing: 5px;
     margin-bottom: 90px;
     
