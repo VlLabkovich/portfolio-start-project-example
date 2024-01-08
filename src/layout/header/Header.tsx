dd63583f0ca05b4bae70ff3c5b3ebb0968a0c1a9
Vladislav <labkovich.vl@gmail.com>
2024-01-08 11:29:53 +0300

    feat: add hooks

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import styled from "styled-components";
+import React, {FC} from 'react';
 import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {HeaderMenu} from "./headerMenu/HeaderMenu";
+import {DesktopMenu} from "./desktopMenu/DesktopMenu";
 import {MobileMenu} from "./mobileMenu/MobileMenu";
+import {S} from "./Header_Styles"
 
 const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact'];
 
-export const Header = () => {
+export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 768;
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth)
+        window.addEventListener("resize", handleWindowResize);
+
+        // Return a function from the effect that removes the event listener
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
     return (
-        <StyledHeader>
+        <S.Header>
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo/>
-                    <HeaderMenu menuItems={items}/>
-                    <MobileMenu menuItems={items}/>
+                    {width < breakpoint ? <MobileMenu menuItems={items}/> : <DesktopMenu menuItems={items}/>}
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
-);
+        </S.Header>
+    );
 };
-
-const StyledHeader = styled.header`
-background: rgba(31, 31, 32, 0.9);
-    padding: 20px 0;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 99999;
-`
