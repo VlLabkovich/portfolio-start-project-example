ab9116ff7570918db9ac03532c73054b2d7abc26
Vladislav <labkovich.vl@gmail.com>
2024-01-09 00:46:39 +0300

    refactor: change structure component

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,68 +1,23 @@
 import React from 'react';
-import styled from "styled-components";
 import {FlexWrapper} from "../FlexWrapper";
-import {theme} from "../../styles/Theme";
+import {S} from "./Slider_styles"
 
-export const Slider = () => {
+export const Slider: React.FC = () => {
     return (
-        <StyledSlider>
+        <S.Slider>
             <FlexWrapper>
-                <Slide>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                <S.Slide>
+                    <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit.</Text>
-                    <Name>@ivan ivanov</Name>
-                </Slide>
+                        elit.</S.Text>
+                    <S.Name>@ivan ivanov</S.Name>
+                </S.Slide>
             </FlexWrapper>
-            <Pagination>
+            <S.Pagination>
                 <span></span>
                 <span className={"active"}></span>
                 <span></span>
-            </Pagination>
-        </StyledSlider>
+            </S.Pagination>
+        </S.Slider>
     );
 };
-
-const StyledSlider = styled.div`
-    max-width: 500px;
-`
-
-const Slide = styled.div`
-    text-align: center;
-`
-
-const Text = styled.p`
-
-`
-
-const Name = styled.span`
-    display: inline-block;
-    margin: 22px 0 42px 0;
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 16px;
-    font-weight: 600;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-`
-
-const Pagination = styled.span`
-    display: flex;
-    justify-content: center;
-
-    span {
-        display: inline-block;
-        width: 7px;
-        height: 7px;
-        border-radius: 20px;
-        background: rgba(255, 255, 255, 0.50);
-
-        & + span {
-            margin-left: 5px;
-        }
-
-        &.active {
-            background-color: ${theme.colors.accent};
-            width: 20px;
-        }
-    }
-`
